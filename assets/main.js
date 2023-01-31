6cef966df349dcd60a0119a38341675a9e7576fb
Jana <janagonzalez.f@gmail.com>
2023-01-31 11:15:22 -0300

    formato

--- assets/main.js
+++ assets/main.js
@@ -17,36 +17,37 @@ const btnAgregar = document.querySelector('#btnAgregar');
 formulario.addEventListener('submit', validarFormulario);
 
 document.addEventListener('DOMContentLoaded', () => {
-    if(localStorage.getItem('listado_tareas') == null) {
-        localStorage.setItem('listado_tareas',JSON.stringify([]));
-    }else   
+    if (localStorage.getItem('listado_tareas') == null) {
+        localStorage.setItem('listado_tareas', JSON.stringify([]));
+    } else
         mostrarTarea();
-})  
+})
 
-
+ 
 function validarFormulario(e) {
     e.preventDefault();
-    
+
+     
     if (nombreTareaInput.value === '' || descripcionTareaInput.value === '') {
         alert('Debe ingresar valores en ambos campos');
         return;
     }
-    
+
     if (editando) {
         editarTarea();
         editando = false;
     }
     else {
-       lista_tareas_localStorage = JSON.parse(localStorage.getItem('listado_tareas'));
-       const idTarea = 'id_' + Date.now();
-       
-       lista_tareas_localStorage.push({
+        lista_tareas_localStorage = JSON.parse(localStorage.getItem('listado_tareas'));
+        const idTarea = 'id_' + Date.now();
+
+        lista_tareas_localStorage.push({
             id: idTarea,
-            tarea : nombreTareaInput.value,
+            tarea: nombreTareaInput.value,
             descripcion: descripcionTareaInput.value
         });
 
-        localStorage.setItem('listado_tareas',JSON.stringify(lista_tareas_localStorage));
+        localStorage.setItem('listado_tareas', JSON.stringify(lista_tareas_localStorage));
         mostrarTarea();
     }
 }
@@ -54,23 +55,23 @@ function validarFormulario(e) {
 
 
 function mostrarTarea() {
-    const divTarea = document.querySelector ('.div-tareas');
+    const divTarea = document.querySelector('.div-tareas');
     divTarea.innerHTML = '';
     let lista_tareas_localStorage = JSON.parse(localStorage.getItem('listado_tareas'));
-    
-    lista_tareas_localStorage.forEach (miTarea => { 
-        const {id,tarea, descripcion} = miTarea;
+
+    lista_tareas_localStorage.forEach(miTarea => {
+        const { id, tarea, descripcion } = miTarea;
         const parrafo = document.createElement('p');
-        parrafo.textContent = tarea + " " + descripcion ;
-        
+        parrafo.textContent = tarea + " " + descripcion;
+
         const editarBoton = document.createElement('button');
-        editarBoton.onclick = () => cargarTarea (miTarea);
+        editarBoton.onclick = () => cargarTarea(miTarea);
         editarBoton.textContent = 'Editar';
         editarBoton.classList.add('btn', 'btn-editar');
         parrafo.append(editarBoton);
 
         const eliminarBoton = document.createElement('button');
-        eliminarBoton.onclick = () => eliminarTarea (id);
+        eliminarBoton.onclick = () => eliminarTarea(id);
         eliminarBoton.textContent = 'Eliminar';
         eliminarBoton.classList.add('btn', 'btn-eliminar');
         parrafo.append(eliminarBoton);
@@ -80,49 +81,49 @@ function mostrarTarea() {
         divTarea.appendChild(parrafo);
         divTarea.appendChild(hr);
     })
-    
+
 }
 
-function cargarTarea(miTarea){
-    const {id,tarea, descripcion} = miTarea;
+function cargarTarea(miTarea) {
+    const { id, tarea, descripcion } = miTarea;
 
     nombreTareaInput.value = tarea;
     descripcionTareaInput.value = descripcion;
     objetoTarea.id = id;
 
     formulario.querySelector('button[type="submit"]').textContent = 'Actualizar';
-    
+
     editando = true;
 
 }
 
-function editarTarea(){
+function editarTarea() {
     objetoTarea.tarea = nombreTareaInput.value;
     objetoTarea.descripcion = descripcionTareaInput.value;
 
     lista_tareas_localStorage = JSON.parse(localStorage.getItem('listado_tareas'));
     const indice = lista_tareas_localStorage.findIndex(tarea => tarea.id === objetoTarea.id);
-    
+
     lista_tareas_localStorage[indice].tarea = objetoTarea.tarea;
     lista_tareas_localStorage[indice].descripcion = objetoTarea.descripcion;
-    localStorage.setItem('listado_tareas',JSON.stringify(lista_tareas_localStorage));
+    localStorage.setItem('listado_tareas', JSON.stringify(lista_tareas_localStorage));
 
     mostrarTarea();
     formulario.reset();
 
     formulario.querySelector('button[type="submit"]').textContent = 'Agregar';
-    
+
     editando = false;
 
 
 }
 
-function eliminarTarea(id){
+function eliminarTarea(id) {
     lista_tareas_localStorage = JSON.parse(localStorage.getItem('listado_tareas'));
     const indice = lista_tareas_localStorage.findIndex(tarea => tarea.id === id);
-    lista_tareas_localStorage.splice(indice,1);
-    localStorage.setItem('listado_tareas',JSON.stringify(lista_tareas_localStorage));
-    
+    lista_tareas_localStorage.splice(indice, 1);
+    localStorage.setItem('listado_tareas', JSON.stringify(lista_tareas_localStorage));
+
     mostrarTarea();
 }
 
